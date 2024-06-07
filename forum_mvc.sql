7b5a05a93b91246b66a39636577ac4534a109e22
seitvalieva <sseitvalieva@gmail.com>
2024-06-07 11:02:42 +0200

    initial

---
+++ forum_mvc.sql
@@ -0,0 +1,18 @@
+
+
+-- Dumping database structure for forum_sevilia
+CREATE DATABASE IF NOT EXISTS `forum_sevilia` /*!40100 DEFAULT CHARACTER SET latin1 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `forum_sevilia`;
+
+-- Dumping structure for table forum_sevilia.forum_user
+
+-- Dumping data for table forum_sevilia.forum_user: ~0 rows (approximately)
+
+-- Dumping structure for table forum_sevilia.message
+
+-- Dumping data for table forum_sevilia.message: ~0 rows (approximately)
+
+-- Dumping structure for table forum_sevilia.topic
+
+-- Dumping data for table forum_sevilia.topic: ~0 rows (approximately)
+
