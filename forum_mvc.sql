d8f1f4d887ac0249266f2c1fc0a42c3f3676601d
seitvalieva <seitvalieva@gmail.com>
2024-06-10 17:00:42 +0200

    bdd

--- forum_mvc.sql
+++ forum_mvc.sql
@@ -1,18 +1,75 @@
+-- --------------------------------------------------------
+-- Hôte:                         127.0.0.1
+-- Version du serveur:           8.0.30 - MySQL Community Server - GPL
+-- SE du serveur:                Win64
+-- HeidiSQL Version:             12.1.0.6537
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
--- Dumping database structure for forum_sevilia
+-- Listage de la structure de la base pour forum_sevilia
 CREATE DATABASE IF NOT EXISTS `forum_sevilia` /*!40100 DEFAULT CHARACTER SET latin1 */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `forum_sevilia`;
 
--- Dumping structure for table forum_sevilia.forum_user
+-- Listage de la structure de table forum_sevilia. message
+CREATE TABLE IF NOT EXISTS `message` (
+  `id_message` int NOT NULL AUTO_INCREMENT,
+  `textMessage` text NOT NULL,
+  `postDate` datetime DEFAULT NULL,
+  `user_id` int NOT NULL,
+  `topic_id` int NOT NULL,
+  PRIMARY KEY (`id_message`),
+  KEY `user_id` (`user_id`),
+  KEY `topic_id` (`topic_id`),
+  CONSTRAINT `FK_message_topic` FOREIGN KEY (`topic_id`) REFERENCES `topic` (`id_topic`),
+  CONSTRAINT `FK_message_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
--- Dumping data for table forum_sevilia.forum_user: ~0 rows (approximately)
+-- Listage des données de la table forum_sevilia.message : ~1 rows (environ)
+INSERT INTO `message` (`id_message`, `textMessage`, `postDate`, `user_id`, `topic_id`) VALUES
+	(1, 'test', NULL, 2, 1);
 
--- Dumping structure for table forum_sevilia.message
+-- Listage de la structure de table forum_sevilia. topic
+CREATE TABLE IF NOT EXISTS `topic` (
+  `id_topic` int NOT NULL AUTO_INCREMENT,
+  `titleTopic` text NOT NULL,
+  `publicationDate` datetime DEFAULT NULL,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`id_topic`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `FK_topic_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
--- Dumping data for table forum_sevilia.message: ~0 rows (approximately)
+-- Listage des données de la table forum_sevilia.topic : ~2 rows (environ)
+INSERT INTO `topic` (`id_topic`, `titleTopic`, `publicationDate`, `user_id`) VALUES
+	(1, 'Topic 1', '2024-06-10 11:57:13', 1),
+	(2, 'Topic 2', '2024-06-10 11:57:20', 1);
 
--- Dumping structure for table forum_sevilia.topic
+-- Listage de la structure de table forum_sevilia. user
+CREATE TABLE IF NOT EXISTS `user` (
+  `id_user` int NOT NULL AUTO_INCREMENT,
+  `pseudo` varchar(50) NOT NULL DEFAULT '0',
+  `email` varchar(255) NOT NULL DEFAULT '0',
+  `password` varchar(255) NOT NULL DEFAULT '0',
+  `registrationDate` date DEFAULT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
--- Dumping data for table forum_sevilia.topic: ~0 rows (approximately)
+-- Listage des données de la table forum_sevilia.user : ~2 rows (environ)
+INSERT INTO `user` (`id_user`, `pseudo`, `email`, `password`, `registrationDate`) VALUES
+	(1, 'stephane', 'stephane@exemple.com', 'aaa', '2024-06-10'),
+	(2, 'user 2', 'user@example.com', 'xxx', '2024-06-10');
 
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
