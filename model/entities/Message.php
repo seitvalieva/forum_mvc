64ff6733e84ca3fdca86e2f00cc85cf44d57e7d4
seitvalieva <seitvalieva@gmail.com>
2024-06-10 11:42:54 +0200

    added user and message entities

--- model/entities/Message.php
+++ model/entities/Message.php
@@ -7,13 +7,14 @@ use App\Entity;
     En programmation orientée objet, une classe finale (final class) est une classe que vous ne pouvez pas étendre, c'est-à-dire qu'aucune autre classe ne peut hériter de cette classe. En d'autres termes, une classe finale ne peut pas être utilisée comme classe parente.
 */
 
-final class Topic extends Entity{
+final class Message extends Entity{
 
     private $id;
-    private $titleTopic;
+    private $textMessage;
     // private $category;
-    private $publicationDate;
+    private $postDate;
     private $user;              // cle etrangere  user_id
+    private $topic;             // cle etrangere  topic_id
     // private $closed;
 
     public function __construct($data){         
@@ -36,6 +37,44 @@ final class Topic extends Entity{
         $this->id = $id;
         return $this;
     }
+    /**
+     * Get the value of textMessage
+     */ 
+    public function getTextMessage()
+    {
+        return $this->textMessage;
+    }
+
+    /**
+     * Set the value of textMessage
+     *
+     * @return  self
+     */ 
+    public function setTextMessage($textMessage)
+    {
+        $this->textMessage = $textMessage;
+
+        return $this;
+    }
+    /**
+     * Get the value of postDate
+     */ 
+    public function getPostDate()
+    {
+        return $this->postDate;
+    }
+
+    /**
+     * Set the value of postDate
+     *
+     * @return  self
+     */ 
+    public function setPostDate($postDate)
+    {
+        $this->postDate = $postDate;
+
+        return $this;
+    }
 
     /**
      * Get the value of user
@@ -54,47 +93,31 @@ final class Topic extends Entity{
         return $this;
     }
     /**
-     * Get the value of titleTopic
+     * Get the value of topic
      */ 
-    public function getTitleTopic()
+    public function getTopic()
     {
-        return $this->titleTopic;
+        return $this->topic;
     }
 
     /**
-     * Set the value of titleTopic
+     * Set the value of topic
      *
      * @return  self
      */ 
-    public function setTitleTopic($titleTopic)
+    public function setTopic($topic)
     {
-        $this->titleTopic = $titleTopic;
-
-        return $this;
-    }
-    /**
-     * Get the value of publicationDate
-     */ 
-    public function getPublicationDate()
-    {
-        return $this->publicationDate;
-    }
-
-    /**
-     * Set the value of publicationDate
-     *
-     * @return  self
-     */ 
-    public function setPublicationDate($publicationDate)
-    {
-        $this->publicationDate = $publicationDate;
+        $this->topic = $topic;
 
         return $this;
     }
     
     public function __toString(){
-        return $this->titleTopic;
+        return $this->textMessage;
     }
+
+
+
 
 
 }
--- model/entities/User.php
+++ model/entities/User.php
@@ -10,7 +10,10 @@ use App\Entity;
 final class User extends Entity{
 
     private $id;
-    private $nickName;
+    private $pseudo;
+    private $email;
+    private $password;
+    private $registrationDate;
 
     public function __construct($data){         
         $this->hydrate($data);        
@@ -36,8 +39,8 @@ final class User extends Entity{
     /**
      * Get the value of nickName
      */ 
-    public function getNickName(){
-        return $this->nickName;
+    public function getPseudo(){
+        return $this->pseudo;
     }
 
     /**
@@ -45,13 +48,73 @@ final class User extends Entity{
      *
      * @return  self
      */ 
-    public function setNickName($nickName){
-        $this->nickName = $nickName;
+    public function setPseudo($pseudo){
+        $this->pseudo = $pseudo;
 
         return $this;
     }
 
+    /**
+     * Get the value of email
+     */ 
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */ 
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of password
+     */ 
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set the value of password
+     *
+     * @return  self
+     */ 
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of registrationDate
+     */ 
+    public function getRegistrationDate()
+    {
+        return $this->registrationDate;
+    }
+
+    /**
+     * Set the value of registrationDate
+     *
+     * @return  self
+     */ 
+    public function setRegistrationDate($registrationDate)
+    {
+        $this->registrationDate = $registrationDate;
+
+        return $this;
+    }
+    
     public function __toString() {
-        return $this->nickName;
+        return $this->pseudo;
     }
 }
