c14a04b2a2d75831becedb2afc610b8aee813cee
seitvalieva <sseitvalieva@gmail.com>
2024-06-11 09:47:55 +0200

    bd

--- forum_sevilia.sql
+++ forum_sevilia.sql
@@ -1,7 +1,7 @@
 -- --------------------------------------------------------
--- Hôte:                         127.0.0.1
--- Version du serveur:           8.0.30 - MySQL Community Server - GPL
--- SE du serveur:                Win64
+-- Host:                         127.0.0.1
+-- Server version:               8.0.30 - MySQL Community Server - GPL
+-- Server OS:                    Win64
 -- HeidiSQL Version:             12.1.0.6537
 -- --------------------------------------------------------
 
@@ -15,58 +15,62 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
--- Listage de la structure de la base pour forum_sevilia
+-- Dumping database structure for forum_sevilia
 CREATE DATABASE IF NOT EXISTS `forum_sevilia` /*!40100 DEFAULT CHARACTER SET latin1 */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `forum_sevilia`;
 
--- Listage de la structure de table forum_sevilia. message
+-- Dumping structure for table forum_sevilia.message
 CREATE TABLE IF NOT EXISTS `message` (
   `id_message` int NOT NULL AUTO_INCREMENT,
   `textMessage` text NOT NULL,
   `postDate` datetime DEFAULT NULL,
-  `user_id` int NOT NULL,
+  `user_id` int NOT NULL DEFAULT '0',
   `topic_id` int NOT NULL,
   PRIMARY KEY (`id_message`),
-  KEY `user_id` (`user_id`),
-  KEY `topic_id` (`topic_id`),
-  CONSTRAINT `FK_message_topic` FOREIGN KEY (`topic_id`) REFERENCES `topic` (`id_topic`),
-  CONSTRAINT `FK_message_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id_user`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  KEY `id_user` (`user_id`) USING BTREE,
+  KEY `id_topic` (`topic_id`) USING BTREE,
+  CONSTRAINT `FK__forum_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id_user`),
+  CONSTRAINT `FK_message_topic` FOREIGN KEY (`topic_id`) REFERENCES `topic` (`id_topic`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
--- Listage des données de la table forum_sevilia.message : ~1 rows (environ)
+-- Dumping data for table forum_sevilia.message: ~3 rows (approximately)
 INSERT INTO `message` (`id_message`, `textMessage`, `postDate`, `user_id`, `topic_id`) VALUES
-	(1, 'test', NULL, 2, 1);
+	(1, 'hello', NULL, 1, 2),
+	(2, 'hola', NULL, 3, 1),
+	(3, 'salut', NULL, 2, 3);
 
--- Listage de la structure de table forum_sevilia. topic
+-- Dumping structure for table forum_sevilia.topic
 CREATE TABLE IF NOT EXISTS `topic` (
   `id_topic` int NOT NULL AUTO_INCREMENT,
-  `titleTopic` text NOT NULL,
+  `titleTopic` varchar(50) NOT NULL DEFAULT '0',
   `publicationDate` datetime DEFAULT NULL,
   `user_id` int NOT NULL,
   PRIMARY KEY (`id_topic`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `FK_topic_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id_user`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  KEY `id_user` (`user_id`) USING BTREE,
+  CONSTRAINT `FK_topic_forum_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
--- Listage des données de la table forum_sevilia.topic : ~2 rows (environ)
+-- Dumping data for table forum_sevilia.topic: ~3 rows (approximately)
 INSERT INTO `topic` (`id_topic`, `titleTopic`, `publicationDate`, `user_id`) VALUES
-	(1, 'Topic 1', '2024-06-10 11:57:13', 1),
-	(2, 'Topic 2', '2024-06-10 11:57:20', 1);
+	(1, 'topic es', NULL, 3),
+	(2, 'topic en', NULL, 1),
+	(3, 'topic fr', NULL, 2);
 
--- Listage de la structure de table forum_sevilia. user
+-- Dumping structure for table forum_sevilia.user
 CREATE TABLE IF NOT EXISTS `user` (
   `id_user` int NOT NULL AUTO_INCREMENT,
   `pseudo` varchar(50) NOT NULL DEFAULT '0',
   `email` varchar(255) NOT NULL DEFAULT '0',
   `password` varchar(255) NOT NULL DEFAULT '0',
-  `registrationDate` date DEFAULT NULL,
+  `registrationDate` datetime DEFAULT NULL,
   PRIMARY KEY (`id_user`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
--- Listage des données de la table forum_sevilia.user : ~2 rows (environ)
+-- Dumping data for table forum_sevilia.user: ~3 rows (approximately)
 INSERT INTO `user` (`id_user`, `pseudo`, `email`, `password`, `registrationDate`) VALUES
-	(1, 'stephane', 'stephane@exemple.com', 'aaa', '2024-06-10'),
-	(2, 'user 2', 'user@example.com', 'xxx', '2024-06-10');
+	(1, 'user en', 'en@email.com', 'xxx', NULL),
+	(2, 'user fr', 'fr@email.com', 'xxx', NULL),
+	(3, 'user es', 'es@email.com', 'xxx', NULL);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
